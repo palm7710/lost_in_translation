b559fb47f5685fabca437cd2ded5cdc3ce75481d
Palm <241it002i@s.internet.ac.jp>
2025-09-28 15:56:22 +0900

    コサイン類似度を計算する為の関数を作成した

--- common/util.py
+++ common/util.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def preprocess(text):
     text = text.lower()
     text = text.replace('.', ' .')
@@ -17,6 +16,32 @@ def preprocess(text):
     
     corpus = np.array([word_to_id[w] for w in words])
     return corpus, word_to_id, id_to_word
+
+# コーパスから共起行列を作る関数
+def create_co_matrix(corpus, vocab_size, window_size=1):
+    corpus_size = len(corpus)
+    co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32) # 要素が０の２次元配列で初期化
+
+    for idx, word_id in enumerate(corpus):
+        for i in range(1, window_size + 1):
+            left_idx = idx - i
+            right_idx = idx + i
+
+            if left_idx >= 0:
+                left_word_id = corpus[left_idx]
+                co_matrix[word_id, left_word_id] += 1
+
+            if right_idx < corpus_size:
+                right_word_id = corpus[right_idx]
+                co_matrix[word_id, right_word_id] += 1
+
+    return co_matrix
+
+# コサイン類似度
+def cos_similarity(x, y, eps=1e-8):
+    nx = x / (np.sqrt(np.sum(x ** 2)) + eps)
+    ny = y / (np.sqrt(np.sum(y ** 2)) + eps)
+    return np.dot(nx, ny)
 
 text = 'You say goodbye and I say hello.'
 corpus, word_to_id, id_to_word = preprocess(text)
@@ -36,5 +61,6 @@ C = np.array([
     [0,0,0,0,0,1,0],
 ], dtype=np.int32)
 
-print(C[0]) # 単語IDが0のベクトル
-print(C[4]) # 単語IDが4のベクトル
+# print(C[0]) # 単語IDが0のベクトル
+# print(C[4]) # 単語IDが4のベクトル
+# print(C[word_to_id['goodbye']]) # [goodbye]のベクトル
